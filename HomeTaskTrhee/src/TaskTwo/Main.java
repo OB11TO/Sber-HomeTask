c4125b31f2501f78aa644bed68c2f14fba2ea842
OB11TO <OB11TO61@yandex.ru>
2020-12-21 17:37:41 +0300

    added CollectionUtils

---
+++ HomeTaskTrhee/src/TaskTwo/Main.java
@@ -0,0 +1,18 @@
+package TaskTwo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Задание 2
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("must be true " + CollectionUtils.containsAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+        System.out.println("must be true " + CollectionUtils.containsAny(Arrays.asList(1, 2, 3, 4, 5, 6, 7), Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+        System.out.println("must be false " + CollectionUtils.containsAny(Arrays.asList(1, 2, 3, 4, 5, 6, 7), Arrays.asList(8, 9, 10)));
+        System.out.println(CollectionUtils.range(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 1, 7));
+    }
+}
