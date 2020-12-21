9ad13560facce42a4a44d1d6c45464d153376cb4
OB11TO <OB11TO61@yandex.ru>
2020-12-21 12:19:46 +0300

    added func

---
+++ HomeTaskTwo/src/InputLines.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class InputLines {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/home/obiito/c/IdeaProjects/Sber-HomeTask/HomeTaskTwo/src/text.txt");
+        Scanner scanner = new Scanner(file);
+    }
+}
