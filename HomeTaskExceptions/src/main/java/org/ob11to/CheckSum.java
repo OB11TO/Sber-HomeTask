d3768e2fc672495e7058879c2ec2bb84ad263c72
OB11TO <OB11TO61@yandex.ru>
2021-01-10 22:35:30 +0300

    added func

---
+++ HomeTaskExceptions/src/main/java/org/ob11to/CheckSum.java
@@ -0,0 +1,22 @@
+package org.ob11to;
+
+import java.util.Scanner;
+
+public class CheckSum extends Main{
+
+    public static void checkSum(int num) {
+        terminal = new TerminalImpl();
+        console = new Scanner(System.in);
+
+        System.out.println("Введите сумму: ");
+        int count = console.nextInt();
+
+        if(num == 1) {
+            terminal.putMoney(count);
+        }else{
+            terminal.getMoney(count);
+        }
+
+    }
+
+}
