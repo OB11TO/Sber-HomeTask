7036f5601d98a19db1134efcd25b601d02479b08
OB11TO <OB11TO61@yandex.ru>
2021-01-10 17:29:39 +0300

    added PinValidator

---
+++ HomeTaskExceptions/src/main/java/org/ob11to/PinValidatorImpl.java
@@ -0,0 +1,41 @@
+package org.ob11to;
+
+import org.ob11to.exceptions.AccountIsLockedException;
+import org.ob11to.exceptions.IncorrectlyEnteredThePinCodeException;
+import org.ob11to.interfaces.PinValidator;
+
+import java.util.Date;
+
+public class PinValidatorImpl implements PinValidator {
+    private final int pinCodeClient = 1111;  //пин код клиента
+    private int pinError;  //ошибочный пин
+    private long dateError;  //временная дата для ошибки
+
+
+    @Override
+    public void verifyPin(int clientIdPin) throws AccountIsLockedException, IncorrectlyEnteredThePinCodeException{
+        Date dateNow = new Date();  // берем новое время
+
+        if(pinError >= 3){
+            dateError = new Date().getTime() + 5000;
+            pinError = 0;
+            throw new AccountIsLockedException();
+        }
+
+        if (pinCodeClient != clientIdPin) { //проверка на пин код
+            pinError++;
+            throw new IncorrectlyEnteredThePinCodeException(); //бросаем исключение, что пин код не верен
+        }
+
+    }
+
+    @Override
+    public void getCountPinError() {
+
+    }
+
+    @Override
+    public void getBankAccountPin() {
+
+    }
+}
