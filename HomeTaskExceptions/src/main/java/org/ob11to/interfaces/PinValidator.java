a367bc9f85823c4b2ea47138faec37096bb673f1
OB11TO <OB11TO61@yandex.ru>
2021-01-10 17:30:32 +0300

    added throws exception

--- HomeTaskExceptions/src/main/java/org/ob11to/interfaces/PinValidator.java
+++ HomeTaskExceptions/src/main/java/org/ob11to/interfaces/PinValidator.java
@@ -1,11 +1,14 @@
 package org.ob11to.interfaces;
 
+import org.ob11to.exceptions.AccountIsLockedException;
+import org.ob11to.exceptions.IncorrectlyEnteredThePinCodeException;
+
 /**
  * Интерфейс для проверки Pin
  */
 public interface PinValidator {
 
-    void verifyPin(int clientIdPin); //функция проверки пин
+    void verifyPin(int clientIdPin) throws AccountIsLockedException, IncorrectlyEnteredThePinCodeException; //функция проверки пин
 
     void getCountPinError();  //??
 
