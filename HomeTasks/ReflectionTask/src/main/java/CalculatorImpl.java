0916ff94e3d4d3896a2656165917c0eae5b41382
OB11TO <OB11TO61@yandex.ru>
2021-01-16 15:36:51 +0300

    add task 1

---
+++ HomeTasks/ReflectionTask/src/main/java/CalculatorImpl.java
@@ -0,0 +1,12 @@
+
+
+public class CalculatorImpl implements Calculator{
+    @Override
+    public int calc(int number) {
+        int r = 1;
+        for(int i = 2; i <=number; i++){
+            r *= i;
+        }
+        return r;
+    }
+}
