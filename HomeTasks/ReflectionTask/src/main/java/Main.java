390e753e6ba735ba86f04cdc8ea149dd23d8713a
OB11TO <OB11TO61@yandex.ru>
2021-01-16 15:37:42 +0300

    add func main

---
+++ HomeTasks/ReflectionTask/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Method;
+import java.lang.Class;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new CalculatorImpl();
+       // Calculator myProxy = (Calculator) MyProxy.newInstance(calculator);
+        System.out.println("Test calculator");
+        System.out.println(calculator.calc(4));
+
+        System.out.println("Task 2");
+        Class<? super CalculatorImpl> clazz = CalculatorImpl.class.getSuperclass();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            method.setAccessible(true);
+            System.out.println(method);
+        }
+
+        System.out.println("Task 3");
+        for(Method method : methods){
+                if(isGetter(method)) System.out.println("getter: " + method);
+        }
+    }
+
+    public static boolean isGetter(Method method){
+        if (!method.getName().startsWith("get")) {
+            return false;
+        }
+        if (method.getParameterTypes().length != 0) {
+            return false;
+        }
+        return !void.class.equals(method.getReturnType());
+    }
+}
