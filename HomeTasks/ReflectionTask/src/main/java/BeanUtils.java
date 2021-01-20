e31c0404abfa7ed14eddfcdf0ea97839c9d53fe6
OB11TO <OB11TO61@yandex.ru>
2021-01-20 08:44:41 +0300

    added func BeanUtils

--- HomeTasks/ReflectionTask/src/main/java/BeanUtils.java
+++ HomeTasks/ReflectionTask/src/main/java/BeanUtils.java
@@ -1,9 +1,6 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class BeanUtils {
     /**
@@ -23,9 +20,31 @@ public class BeanUtils {
      * @param from Object which properties will be used to get values.
      */
     public static void assign(Object to, Object from) {
-        Class classTo = to.getClass();
-        Class classFrom = from.getClass();
+        Class<?> classTo = to.getClass();
+        Class<?>  classFrom = from.getClass();
 
+        for(Method method : classFrom.getMethods()){
+            if(method.getName().startsWith("get")){
+                System.out.println("Геттер" + method);
+            }
+            String setterName = method.getName().replace("get", "set");
+            System.out.println("Поиск сеттера: " + setterName);
 
+            for(Method m : classTo.getMethods()){
+                if(m.getName().equals(setterName)){
+                    if (method.getReturnType().isAssignableFrom(m.getParameterTypes()[0])) {
+                        System.out.println("Найден сеттер: " + m);
+                        try {
+                            m.invoke(to, method.invoke(from));
+                            System.out.println("Значение установлено.");
+                        } catch (IllegalAccessException e) {
+                            System.out.println("Нет доступа к методу " + method);
+                        } catch (InvocationTargetException e) {
+                            System.out.println("Возникли проблемы с вызовом метода " + method);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
